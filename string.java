d637b4746c5459e985fe693472ce74f57d519d92
binnygarg98 <binnygarg7@gmail.com>
2019-11-15 02:27:08 +0530

    string done

---
+++ string.java
@@ -0,0 +1,33 @@
+package Lcm;
+import java.util.*;
+public class string
+{
+public static void main(String arg[])
+{
+	Scanner in=new Scanner(System.in);
+	String str=in.next();
+	int len=str.length();
+	char arr[]=str.toCharArray();
+	int i=0;
+	int j=len-1;
+	while(i<j)
+	{
+		if((arr[i]<65) || (arr[i]>90 && arr[i]<97) || (arr[i]>122))
+		{
+			i++;
+			continue;
+		}
+		if((arr[j]<65) || (arr[j]>90 && arr[j]<97) || (arr[j]>122))
+		{
+			j--;
+			continue;
+		}
+		char temp=arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
+		i++;
+		j--;
+	}
+	System.out.println(arr);
+}
+}
