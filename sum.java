ac52426b9ba699256b07b1735207cb8bb319cc80
binnygarg98 <binnygarg7@gmail.com>
2019-12-13 13:20:49 +0530

    1done done

---
+++ sum.java
@@ -0,0 +1,17 @@
+package Lcm;
+import java.util.*;
+public class add 
+{
+public static void main(String arg[])
+{
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	int b=in.nextInt();
+	while(a!=0)
+	{
+		a--;
+		b++;
+	}
+	System.out.println(b);
+}
+}
